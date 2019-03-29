f34019a47370a8183a07b4ad911ac4e43625c012
Franziska Schmidt <franka.schmidt@mapbox.com>
2019-03-29 16:26:39 -0300

    format things

--- www/index.js
+++ www/index.js
@@ -49,33 +49,33 @@ const drawGrid = () => {
 };
 const getIndex = (row, column) => {
   return row * width + column;
-  };
+};
 
-  const drawCells = () => {
-    const cellsPtr = universe.cells();
-      const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
 
-        ctx.beginPath();
+  ctx.beginPath();
 
-          for (let row = 0; row < height; row++) {
-              for (let col = 0; col < width; col++) {
-                    const idx = getIndex(row, col);
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
 
-                          ctx.fillStyle = cells[idx] === Cell.Dead
-                                  ? DEAD_COLOR
-                                          : ALIVE_COLOR;
+      ctx.fillStyle = cells[idx] === Cell.Dead
+        ? DEAD_COLOR
+        : ALIVE_COLOR;
 
-                                                ctx.fillRect(
-                                                        col * (CELL_SIZE + 1) + 1,
-                                                                row * (CELL_SIZE + 1) + 1,
-                                                                        CELL_SIZE,
-                                                                                CELL_SIZE
-                                                                                      );
-                                                                                          }
-                                                                                            }
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
 
-                                                                                              ctx.stroke();
-                                                                                              };
+  ctx.stroke();
+};
 
 drawGrid();
 drawCells();
